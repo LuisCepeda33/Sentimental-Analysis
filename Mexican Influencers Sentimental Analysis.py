9913ca2b7eb59dbe9cd6aec7d9e90b729b07c132
furro33 <75059997+furro33@users.noreply.github.com>
2021-01-22 00:54:47 -0600

    Add files via upload

---
+++ Mexican Influencers Sentimental Analysis.py
@@ -0,0 +1,162 @@
+from textblob import TextBlob
+import sys
+import tweepy
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+import nltk
+import pycountry
+import re
+import string
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from langdetect import detect
+from nltk.stem import SnowballStemmer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# nltk.downloader.download('vader_lexicon')
+
+
+auth = tweepy.AppAuthHandler("U1JtNPSHYJvJikl5jjQCFyBX1", "e3cUrZLWg4ZklkBChWmzG3xdxODdaYjJxDGIUgV37dHOl5pNIq")
+api = tweepy.API(auth)
+
+
+# Sentiment Analysis
+
+def cleantxt(text):
+    text = re.sub(r'@[A-Za-z0-9:_]+', '', text)
+    text = re.sub(r'#', '', text)
+    text = re.sub(r'RT[\s]+', '', text)
+    text = re.sub(r'https?:\/\/\S+', '', text)
+
+    return text
+
+
+def percentage(part, whole):
+    return 100 * float(part)/float(whole)
+
+
+keyword = input("Please enter keyword or hashtag to search: ")
+noOfTweet = int(input("Please enter how many tweets to analyze: "))
+tweets = tweepy.Cursor(api.search, q=keyword).items(noOfTweet)
+positive = 0
+negative = 0
+neutral = 0
+polarity = 0
+tweet_list = []
+neutral_list = []
+negative_list = []
+positive_list = []
+for tweet in tweets:
+
+    # print(tweet.text)
+    tweet_list.append(tweet.text)
+    analysis = TextBlob(tweet.text)
+    score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
+    neg = score["neg"]
+    neu = score["neu"]
+    pos = score["pos"]
+    comp = score["compound"]
+    polarity += analysis.sentiment.polarity
+
+    if neg > pos:
+        negative_list.append(tweet.text)
+        negative += 1
+    elif pos > neg:
+        positive_list.append(tweet.text)
+        positive += 1
+
+    elif pos == neg:
+        neutral_list.append(tweet.text)
+        neutral += 1
+positive = percentage(positive, noOfTweet)
+negative = percentage(negative, noOfTweet)
+neutral = percentage(neutral, noOfTweet)
+polarity = percentage(polarity, noOfTweet)
+positive = format(positive, ".1f")
+negative = format(negative, ".1f")
+neutral = format(neutral, ".1f")
+
+
+# Number of Tweets (Total, Positive, Negative, Neutral)
+tweet_list = pd.DataFrame(tweet_list)
+neutral_list = pd.DataFrame(neutral_list)
+negative_list = pd.DataFrame(negative_list)
+positive_list = pd.DataFrame(positive_list)
+print("total number: ", len(tweet_list))
+print("positive number: ", len(positive_list))
+print("negative number: ", len(negative_list))
+print("neutral number: ", len(neutral_list))
+# print(tweet_list)
+
+# Creating PieCart
+
+labels = ["Positive ["+str(positive)+"%]", "Neutral ["+str(neutral)+"%]", "Negative ["+str(negative)+"%]"]
+sizes = [positive, neutral, negative]
+colors = ["yellowgreen", "blue","red"]
+plt.pie(sizes, colors=colors, startangle=90)
+plt.style.use("default")
+plt.legend(labels)
+plt.title("Sentiment Analysis Result for "+keyword+"")
+plt.axis("equal")
+plt.show()
+
+# Cleaning Data
+# Drop duplicates
+tweet_list.drop_duplicates(inplace=True)
+
+
+# Cleaning Text (RT, Punctuation etc)
+# Creating new dataframe and new features
+tw_list = pd.DataFrame(tweet_list)
+tw_list["text"] = tw_list[0]
+
+
+
+
+
+# Removing RT, Punctuation etc
+tweet_list["text"] = tweet_list["text"].apply(cleantxt)
+print(tweet_list)
+
+#Calculating Negative, Positive, Neutral and Compound values
+tw_list[["polarity", "subjectivity"]] = tw_list["text"].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
+for index, row in tw_list["text"].iteritems():
+    score = SentimentIntensityAnalyzer().polarity_scores(row)
+    neg = score["neg"]
+    neu = score["neu"]
+    pos = score["pos"]
+    comp = score["compound"]
+    if neg > pos:
+        tw_list.loc[index, "sentiment"] = "negative"
+    elif pos > neg:
+        tw_list.loc[index, "sentiment"] = "positive"
+    else:
+        tw_list.loc[index, "sentiment"] = "neutral"
+    tw_list.loc[index, "neg"] = neg
+    tw_list.loc[index, "neu"] = neu
+    tw_list.loc[index, "pos"] = pos
+    tw_list.loc[index, "compound"] = comp
+print(tw_list.head(10))
+tweet_list.to_csv('pruebaputo.csv')
+
+
+#Creating new data frames for all sentiments (positive, negative and neutral)
+tw_list_negative = tw_list[tw_list["sentiment"] == "negative"]
+tw_list_positive = tw_list[tw_list["sentiment"] == "positive"]
+tw_list_neutral = tw_list[tw_list["sentiment"] == "neutral"]
+
+
+def count_values_in_column(data,feature):
+    total=data.loc[:,feature].value_counts(dropna=False)
+    percentage =round(data.loc[:,feature].value_counts(dropna=False,normalize=True)*100,2)
+    return pd.concat([total,percentage],axis=1,keys=["Total","Percentage"])
+    #Count_values for sentiment
+
+
+count_values_in_column(tw_list,"sentiment")
+
+
