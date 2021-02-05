d1d6158573e16e523499a734cf486327e10bfbc1
furro33 <75059997+furro33@users.noreply.github.com>
2021-02-04 23:49:11 -0600

    Add files via upload

---
+++ extract comments users.py
@@ -0,0 +1,40 @@
+import json
+
+# Opening JSON file
+f = open('furro33.json', encoding="utf8")
+data = json.load(f)
+
+print(type(data))
+
+
+info = []
+for x in range(len(data["GraphImages"])):
+    info.append(data["GraphImages"][x]["comments"])
+
+print(info[0]["data"])
+data = []
+for y in range(len(info[0]["data"])):
+    data.append(info[0]["data"][0])
+
+print(data[0])
+
+
+
+#for z in range(len(info)):
+#    print(info[z])
+
+#print(info[0])
+#for t in range(len(info[0]))
+
+
+
+no_empty = []
+for y in range(len(info)):
+    if len(info[y]) == 0:
+        pass
+    else:
+        no_empty.append(info[y])
+
+
+
+
