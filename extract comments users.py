80d3d9c458d20b4955baa2943e0f4c61879966d7
furro33 <75059997+furro33@users.noreply.github.com>
2021-02-11 20:54:06 -0600

    Update extract comments users.py

--- extract comments users.py
+++ extract comments users.py
@@ -1,39 +1,47 @@
 import json
+import re
 
 # Opening JSON file
-f = open('furro33.json', encoding="utf8")
+f = open('samuelgarcias.json', encoding="utf8")
 data = json.load(f)
 
-print(type(data))
-
-#####NA
-info = []
-for x in range(len(data["GraphImages"])):
-    info.append(data["GraphImages"][x]["comments"])
-
-print(info[0]["data"])
-data = []
-for y in range(len(info[0]["data"])):
-    data.append(info[0]["data"][0])
-
-print(data[0])
+#print(type(data))
 
 
+def cleantxt(text):
+    text = re.sub(r'@[A-Za-z0-9:_]+', '', text)
+    text = re.sub(r'#', '', text)
+    text = re.sub(r'RT[\s]+', '', text)
+    text = re.sub(r'https?:\/\/\S+', '', text)
+    return text
 
-#for z in range(len(info)):
-#    print(info[z])
 
-#print(info[0])
-#for t in range(len(info[0]))
+def extract_comments_users(dats):
+    info = []
+    for x in range(len(dats["GraphImages"])):
+        info.append(dats["GraphImages"][x]["comments"]["data"])
+
+    picWcomment = []
+    for i in range(len(info)):
+        if len(info[i]) == 0:
+            pass
+        else:
+            picWcomment.append(info[i])
+
+    a = picWcomment[0]
+
+    comments = []
+    for z in range(len(picWcomment)):
+        for x in range(len(picWcomment[z])):
+            comments.append(picWcomment[z][x]["text"])
+
+    a = comments
+    txtclean = []
+    for i in range(len(a)):
+        txtclean.append(cleantxt(a[i]))
+    return txtclean
 
 
-
-no_empty = []
-for y in range(len(info)):
-    if len(info[y]) == 0:
-        pass
-    else:
-        no_empty.append(info[y])
 
 
 
