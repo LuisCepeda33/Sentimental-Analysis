983da5571ede38c8efda07da87f570524f23812a
furro33 <75059997+furro33@users.noreply.github.com>
2021-02-11 20:54:33 -0600

    Update extract comments tags.py

--- extract comments tags.py
+++ extract comments tags.py
@@ -1,9 +1,16 @@
 import json
-
+import re
 # Opening JSON file
 f = open('honeybadgerlatte.json', encoding="utf8")
 data = json.load(f)
 
+
+def cleantxt(text):
+    text = re.sub(r'@[A-Za-z0-9:_]+', '', text)
+    text = re.sub(r'#', '', text)
+    text = re.sub(r'RT[\s]+', '', text)
+    text = re.sub(r'https?:\/\/\S+', '', text)
+    return text
 
 def extract_comments_tags(dat):
     dic = []
@@ -18,18 +25,16 @@ def extract_comments_tags(dat):
             no_empty.append(dic[y])
 
     comments = []
+
     for z in range(len(no_empty)):
         for x in range(len(no_empty[z])):
             comments.append(no_empty[z][x]["text"])
 
-    return comments
-
-f.close()
-
-
-
-print(extract_comments_tags(data))
-
+    a = comments
+    txtclean = []
+    for i in range(len(a)):
+        txtclean.append(cleantxt(a[i]))
+    return txtclean
 
 
 
